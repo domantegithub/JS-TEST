8dc855ce0608144240ad5d3b4f740941ad35ab06
domantegithub <domante.nav@gmail.com>
2022-07-14 19:31:36 +0300

    format document

--- Task 4/script.js
+++ Task 4/script.js
@@ -11,26 +11,25 @@ turėti bent minimalų stilių ir būti responsive;
 const ENDPOINT = "cars.json";
 
 const renderCars = (car) => {
-    const { brand, models } = car;
-    //Susikuriam naujus elementus (korteles)
-    const carEl = document.createElement("div");
-    carEl.className = "card";
+  const { brand, models } = car;
+  //Susikuriam naujus elementus (korteles)
+  const carEl = document.createElement("div");
+  carEl.className = "card";
   const carBrand = document.createElement("h3");
   const modelsList = document.createElement("ul");
   // //tortele tiesiog viename baltame fone pastilizuotas elementas
-  
+
   carBrand.textContent = brand;
-    
-    car.models.forEach(car => {
-        const list = document.createElement("li");
-        list.textContent = car;
-        modelsList.append(list);
-    });
 
-    carEl.append(carBrand, modelsList);
-    document.querySelector(".output").prepend(carEl);
-  };
+  car.models.forEach((car) => {
+    const list = document.createElement("li");
+    list.textContent = car;
+    modelsList.append(list);
+  });
 
+  carEl.append(carBrand, modelsList);
+  document.querySelector(".output").prepend(carEl);
+};
 
 fetch(ENDPOINT)
   .then((resp) => resp.json())
